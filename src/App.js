e9a227df44371fa938e66be71a15298e51208868
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-05-01 07:35:26 +0600

    done this project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [peoples, setPeople] = useState(data);
+
+  return <main>
+    <section className='container'>
+      <h2>{peoples.length} birthday today</h2>
+      {
+        peoples.map(people => <List people={people} />)
+      }
+      <button onClick={() => setPeople([])}>Clear All</button>
+    </section>
+  </main>
 }
 
 export default App;
